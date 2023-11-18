c060c07f85d00d6524485aed0a6dc53aff6c2227
Godsheritage  Adeoye <Adeoyegodsheritage@gmail.com>
2023-11-17 22:30:25 -0500

    user routing created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Home from "./pages/home/Home";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HeaderComponent from "./components/Header";
+import FooterComponent from "./components/Footer";
 
 const App: React.FC = () => {
-  
-
   return (
-    <Home/>
+    <>
+    {/* THESE SECTIOON CONTROLS THE ROUTING */}
+      <HeaderComponent />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <FooterComponent />
+    </>
   );
 };
 
