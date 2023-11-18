55d519f84b7867d494fbbd0095830a8cf3f1cb3a
Damilola-4th <105187467+Damilola-4th@users.noreply.github.com>
2023-11-18 01:25:13 -0500

    implemented login and signup

--- src/Login/LogIn.tsx
+++ src/Login/LogIn.tsx
@@ -2,16 +2,19 @@ import React from 'react'
 import { useState } from 'react'
 import { Input, Button } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
+import { useNavigate } from 'react-router-dom';
 const LogIn = ({setUserName, userName, setPass, pass, Login,setLogin }) => {
 
   
   const [LoginPass,setLoginPass ] = useState()
   const [LoginUserName ,setLoginUserName ] = useState()
+  let history = useNavigate()
   const HandleLogInSubmission = () => {
     console.log(userName, LoginUserName, LoginPass, pass)
         if (LoginUserName == userName && LoginPass == pass && LoginUserName.length > 0 && pass.length > 0){
         console.log(LoginPass)
         setLogin(true)
+        history("/")
     }
     
   }
